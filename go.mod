3dcfe8ede7211256c634d9eb0022b7a527e8e93b
FourmiSushi <fourmisuhsi@secret.addr>
2019-05-21 10:29:42 +0900

    なんか日本語の入力だと、文字削除がアになる

--- go.mod
+++ go.mod
@@ -2,4 +2,12 @@ module buri
 
 go 1.12
 
-require github.com/mrjones/oauth v0.0.0-20180629183705-f4e24b6d100c
+require (
+	github.com/cenkalti/backoff v2.1.1+incompatible // indirect
+	github.com/dghubble/go-twitter v0.0.0-20190512073027-53f972dc4b06
+	github.com/dghubble/oauth1 v0.5.0
+	github.com/dghubble/sling v1.2.0 // indirect
+	github.com/google/go-querystring v1.0.0 // indirect
+	github.com/mrjones/oauth v0.0.0-20180629183705-f4e24b6d100c
+	github.com/stretchr/testify v1.3.0 // indirect
+)
--- tweet.go
+++ tweet.go
@@ -4,6 +4,9 @@ import (
 	"fmt"
 	"log"
 	"strconv"
+
+	"github.com/dghubble/go-twitter/twitter"
+	"github.com/dghubble/oauth1"
 )
 
 func tweetHandler(argSlice []string) {
@@ -20,10 +23,37 @@ func tweetHandler(argSlice []string) {
 
 func tweetMode(accountNum int) {
 	fmt.Println("tweet mode")
+	toEnd := false
+	for {
+		fmt.Print(" > ")
+
+		body := ""
+		fmt.Scanln(&body)
+
+		if body == "" && toEnd {
+			break
+		}
+		if body == "" {
+			toEnd = true
+		} else {
+			tweet(accountNum, body)
+		}
+	}
 }
 
-func tweet(accountNum int) {
+func tweet(accountNum int, body string) {
 	fmt.Println("tweet")
+	k := getKeys()
+
+	consumer := oauth1.NewConfig(k.ConsumerKey, k.ConsumerSecret)
+	atoken := oauth1.NewToken(userSetting.Accounts[userSetting.DefaultAccount].AccessToken, userSetting.Accounts[userSetting.DefaultAccount].AccessSecret)
+	httpClient := consumer.Client(oauth1.NoContext, atoken)
+	client := twitter.NewClient(httpClient)
+	_, resp, err := client.Statuses.Update(body, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Println(resp.Status)
 }
 
 func showTweetHelp() {
